85b94382edecb34c5faae60fd51f40642468961b
Sangminhong <33138807+Sangminhong@users.noreply.github.com>
2017-12-03 23:12:42 +0000

    push total energy

---
+++ total energy.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (void) {
+double mass = 1;
+double velocity = 2;
+double ke = 0.5*mass*velocity*velocity;
+#define PE 500;
+double total_energy = ke + PE;
+printf("total energy = %f", total_energy);
+return 0;
+}
