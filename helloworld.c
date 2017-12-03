2c2230fa60b439be67170b20c6b07bf0c104d333
Sangminhong <33138807+Sangminhong@users.noreply.github.com>
2017-12-03 21:36:03 +0000

    push helloworld

---
+++ helloworld.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main (void) { //this is the main function
+/* the lines below rpint out `hello world` and `My anme is Sangmin HONG.` */
+  printf("Hello world!\n" "My name is \n <Sangmin HONG>.");
+  return 0;
+}
