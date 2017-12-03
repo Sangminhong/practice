ba193432750c5f6765974b572f87c75f6b4a3c4a
chrissy211 <34220193+chrissy211@users.noreply.github.com>
2017-12-03 23:15:31 +0000

    change made to print-outs

--- total energy.c
+++ total energy.c
@@ -6,6 +6,6 @@ double velocity = 2;
 double ke = 0.5*mass*velocity*velocity;
 #define PE 500;
 double total_energy = ke + PE;
-printf("total energy = %f", total_energy);
+printf("Total Energy = %f", total_energy);
 return 0;
 }
